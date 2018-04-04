56278186de7aea425bec9ad3af59f7c56d778b84
Will Badart <wbadart@live.com>
2018-04-03 22:54:18 -0400

    First pass at kmeans implementation Follows same structure as response to CSE 40647 HW3 response

--- kmeans-hs/KMeans.hs
+++ kmeans-hs/KMeans.hs
@@ -1,5 +1,5 @@
 {--
- - KMeans.hs
+ - kmeans-hs/KMeans.hs
  -
  - Implementation of the K means clustering algorithm.
  -
@@ -8,5 +8,32 @@
  -}
 
 module KMeans
-(
+( minkowski
+, kmeans
 ) where
+
+import Data.Function (on)
+import Data.List (minimumBy)
+
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+
+type Point  = [Double]
+type Metric = (Point -> Point -> Double)
+
+minkowski :: Double -> Point -> Point -> Double
+minkowski p pointA pointB =
+   sum (zipWith absDistP pointA pointB) ** (1 / p)
+  where
+   absDistP xa xb = abs (xa - xb) ** p
+
+closestCentroid :: Point -> Metric -> [Point] -> Point
+closestCentroid point metric = minimumBy (compare `on` metric point)
+
+kmeans :: [Point] -> Metric -> [Point] -> Map Point [Point]
+kmeans centroids metric data_ =
+    foldr assignCluster (M.fromList $ zip centroids (repeat [])) centroids
+  where
+    assignCluster point clustering =
+      let target_centroid = closestCentroid point metric $ M.keys clustering
+      in  M.adjust (point:) target_centroid clustering
